73b9e4d075033c4cace2ff85bb94d786031684c1
BigFootDID <dongigdong@gmail.com>
2025-07-07 19:33:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
     with open("access.log", "a", encoding="utf-8") as f:
         f.write(f"IP: {ip}, UA: {ua}\n")
 
-    return render_template('index.html')
+    return render_template('./index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
