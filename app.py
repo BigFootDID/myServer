cdc4bb89ec91e644cc58223fb2db086a8b3580bf
BigFootDID <dongigdong@gmail.com>
2025-07-07 19:05:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # 클라이언트 정보 추출
+    ip = request.remote_addr
+    ua = request.headers.get('User-Agent')
+    
+    # 로그 파일에 기록 (append 모드)
+    with open("access.log", "a", encoding="utf-8") as f:
+        f.write(f"IP: {ip}, UA: {ua}\n")
+
     return render_template('index.html')
 
 if __name__ == '__main__':
